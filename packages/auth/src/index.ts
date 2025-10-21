4047dbe33007ffbae972a6e23520358cfcaf912b
Francisco Juan <juansoussev@gmail.com>
2025-10-20 22:09:46 -0300

    chore: creating api app. and configurung node tsconfig

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,22 @@
+import { AbilityBuilder, createMongoAbility, type CreateAbility, type ForcedSubject, type MongoAbility } from "@casl/ability";
+
+const actions = ['manage', 'invite', 'delete'] as const;
+const subjects = ['all', 'User'] as const;
+
+type AppAbilities = [
+  (typeof actions)[number],
+  (
+    | (typeof subjects)[number]
+    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
+  )
+]
+
+export type AppAbility = MongoAbility<AppAbilities>
+export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
+
+const { build, can, cannot } = new AbilityBuilder(createAppAbility)
+
+can('invite', 'User')
+cannot('delete', 'User')
+
+export const ability = build()
