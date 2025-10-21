8fddea7503a2cc8848a4c02027f9cd2656459640
Francisco Juan <juansoussev@gmail.com>
2025-10-20 22:24:29 -0300

    feat: creating new files to separate permissons between Projects and Users

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,22 +1,24 @@
 import { AbilityBuilder, createMongoAbility, type CreateAbility, type ForcedSubject, type MongoAbility } from "@casl/ability";
+import type { User } from "./models/user";
+import { permissions } from "./permissions";
+import type { UserSubject } from "./subjects/user";
+import type { ProjectSubject } from "./subjects/project";
 
-const actions = ['manage', 'invite', 'delete'] as const;
-const subjects = ['all', 'User'] as const;
-
-type AppAbilities = [
-  (typeof actions)[number],
-  (
-    | (typeof subjects)[number]
-    | ForcedSubject<Exclude<(typeof subjects)[number], 'all'>>
-  )
-]
+type AppAbilities = UserSubject | ProjectSubject | ['manage', 'all']
 
 export type AppAbility = MongoAbility<AppAbilities>
 export const createAppAbility = createMongoAbility as CreateAbility<AppAbility>
 
-const { build, can, cannot } = new AbilityBuilder(createAppAbility)
+export function defineAbilityFor(user: User) {
+  const builder = new AbilityBuilder(createAppAbility)
 
-can('invite', 'User')
-cannot('delete', 'User')
+  if (typeof permissions[user.role] !== 'function') {
+    throw new Error(`Permissions for role ${user.role} not found.`)
+  }
 
-export const ability = build()
+  permissions[user.role](user, builder)
+
+  const ability = builder.build()
+
+  return ability
+}
